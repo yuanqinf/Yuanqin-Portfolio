533d3f9fbe9308f2b47bc5a87330ca966bbf3dc0
Yuanqin Fan <fanyuanqin96@gmail.com>
2022-01-16 19:30:02 -0800

    fixed typo

--- src/pages/work/Blindspot.js
+++ src/pages/work/Blindspot.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Layout } from '../../layout/Layout';
-import BlindSpotComponent from '../../components/AllProjects/ProjectDetails/BlindSpotComponent';
+import BlindspotComponent from '../../components/AllProjects/ProjectDetails/BlindSpotComponent';
 
 export default function Blindspot() {
     return (
         <Layout>
-            <BlindSpotComponent />
+            <BlindspotComponent />
         </Layout>
     )
 }
